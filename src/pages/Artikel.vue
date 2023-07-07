f8f8ef3cbc860020a9252e5ba47fc3179ba6e802
bilkis38 <bilkisnisa38@gmail.com>
2023-07-07 15:56:04 +0700

    artikel

--- src/pages/Artikel.vue
+++ src/pages/Artikel.vue
@@ -1,40 +1,137 @@
 <template>
   <Layout>
-    <h1>Artikel</h1>
-    <section class="section-card">
-      <div class="card card-1">
-        <g-image src="../gambar/red.jpg" alt="red shoes" class="card__img" width="500"/>
-      <div class="card__details">
-           <ul>Sepatu Merah. </ul>
-      </div>
-      <a href="#" class="btn">Book now</a>
-      </div>
+    <link rel="stylesheet" href="style.css">
+    
+      <header>
+        <h1>Artikel Terbaru</h1>
+      </header>
+    
+    <div class=" container">
 
-      <div class="card card-2">
-        <!-- <g-image src="../gambar/yellow.png" alt="yellow shoes" class="card__img" width="500"/>
-              <ul> ini adalah artikel yang menceritakan sepatu berwarna kuning</ul>
-          </div>
-          <a href="#" class="btn">Book now</a> -->
-      </div>
-
-      <div class="card card-3">
-        <!-- <g-image src="../gambar/green.png" alt="yellow shoes" class="card__img" width="500"/>
-              <ul> ini adalah artikel yang menceritakan sepatu berwarna hijau</ul>
-          </div>
-          <a href="#" class="btn">Book now</a> -->
-      
+        <div class="artikel-1">
+            <div class="card">
+                <article>
+                  <h1>Tujuan Wisata Indonesia yang Populer di Mata Dunia</h1>
+                    <g-image src="../gambar/1.jpg" alt="gambar 1" class="card__img" width="500"/>
+                      <div class="card__details"> </div>
+                      <p>
+                        Banyak daerah di Indonesia yang sangat terkenal sebagai 
+                        destinasi wisata internasional. Berikut 20 daerah di Indonesia 
+                        yang terkenal hingga ke mancanegara.
+                      </p>
+                  <span>ADMIN</span>
+                </article>
+            </div>
         </div>
-    </section>
-</Layout>
+
+        <div class="artikel-2">
+            <div class="card">
+                <article>
+                  <h1>10 Tempat Wisata Unik di Indonesia</h1>
+                      <g-image src="../gambar/2.jpg" alt="gambar 2" class="card__img" width="500"/>
+                        <div class="card__details"> </div>
+                          <p>
+                            Indonesia memiliki destinasi wisata yang sangat berlimpah dari 
+                            Sabang sampai Merauke. Mulai dari wisata religi, edukasi, 
+                            religi, bahari, pegunungan, dan berbagai panorama lainnya bisa Anda temui.
+                          </p>
+                  <span>ADMIN</span>
+                </article>
+            </div>
+        </div>
+
+        <div class="artikel-3">
+            <div class="card">
+                <article>
+                  <h1>10 Objek Wisata Bandung Favorit Wisatawan</h1>
+                      <g-image src="../gambar/3.jpg" alt="gambar 3" class="card__img" width="500"/>
+                        <div class="card__details"> </div>
+                  <p>
+                    Bandung menjadi salah satu tujuan 
+                    wisata favorit bagi banyak orang. 
+                    Suasananya yang sejuk dan asri, 
+                    membuat Bandung sering dijadikan 
+                    sebagai tempat tujuan untuk 
+                    berjalan-jalan dan menenangkan pikiran.
+                  </p>
+                  <span>ADMIN</span>
+                </article>
+            </div>
+        </div>
+        <div class="artikel-4">
+            <div class="card">
+                <article>
+                  <h1>10 Destinasi Wisata Edukasi Terbaik di Indonesia untuk Liburan Keluarga</h1>
+                       <g-image src="../gambar/4.jpg" alt="gambar 4" class="card__img" width="500"/>
+                        <div class="card__details"> </div>
+                        <p>
+                            Ada banyak hal yang bisa kamu dapatkan dalam perjalanan. 
+                            Mulai dari melihat hal-hal baru yang belum pernah 
+                            kamu temukan sebelumnya, sampai berkenalan dengan orang-orang baru
+                        </p>
+                  <span>ADMIN</span>
+                </article>
+            </div>
+        </div>
+
+        <div class="artikel-5">
+            <div class="card">
+                <article>
+                  <h1>10 Tempat Wisata Buatan Terbaik Wajib Dikunjungi di Indonesia</h1>
+                      <g-image src="../gambar/5.jpg" alt="gambar 5" class="card__img" width="500"/>
+                        <div class="card__details"> </div>
+                           <p>
+                              Indonesia terkenal dengan panoramanya yang alami. 
+                              Ada begitu banyak inovasi wisata buatan di Indonesia 
+                              yang bisa menjadi referensi berwisatalokasi wisata buatan bersaing melakukan inovasi-inovasi agar 
+                              tetap menarik perhatian wisatawan.
+                           </p>
+                  <span>ADMIN</span>
+                </article>
+            </div>
+        </div>
+
+        <div class="artikel-6">
+            <div class="card">
+                <article>
+                  <h1>20 Daftar Tempat Wisata di Cirebon yang Wajib Dikunjungi</h1>
+                       <g-image src="../gambar/6.jpg" alt="gambar 6" class="card__img" width="500"/>
+                        <div class="card__details"> </div>
+                  <p>
+                    Cirebon yang merupakan pintu gerbang Provinsi Jawa Barat 
+                    dari wilayah timur Jawa ini kini semakin ramai didatangi wisatawan. 
+                    Ada beberapa destinasi wisata yang cocok buat refreshing, healing, maupun religi. 
+                    Teduhnya pepohonan dan pemandangan alam bisa bikin adem suasana hati kamu yang butuh liburan.
+                  </p>
+                  <span>ADMIN</span>
+                </article>
+            </div>
+        </div>
+
+        <div class="artikel-7">
+            <div class="card">
+              <div class="tumb" style="background-image: url(gambar/7.jpg)"> </div>
+                <article>
+                  <h1>210 Tempat Wisata Terbaik di Indonesia yang Mendunia</h1>
+                  <p>
+                    Tempat wisata terbaik di Indonesia tentu sudah banyak diketehui
+                     wisatawan mancanegara. Bahkan destinasi-destinasi seperti Bali
+                      telah menjadi tempat liburan langganan turis dalam negeri maupun mancanegara. 
+                      Namun, tidak hanya Bali saja tempat wisata yang mengagumkan di Indonesia. 
+                  </p>
+                  <span>ADMIN</span>
+                </article>
+            </div>
+        </div>
+
+      </div>
+  </Layout>
 </template>
 
-  
-</div>
 <script>
 export default {
   metaInfo: {
-    title: 'Artikel'
+    title: 'About us'
   }
 }
 </script>
-
